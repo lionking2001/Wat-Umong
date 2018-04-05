fcf813e1aea70314ec91e1a6b6a94a9528459177
lionking2001 <kitsada_tang@cmu.ac.th>
2018-04-05 19:04:30 +0700

    Refactor code

--- app/src/main/java/com/example/sear_cheulong/watoumong/Home.java
+++ app/src/main/java/com/example/sear_cheulong/watoumong/Home.java
@@ -1,7 +1,7 @@
 package com.example.sear_cheulong.watoumong;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 
 public class Home extends AppCompatActivity {
 
@@ -9,5 +9,8 @@ public class Home extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+
+
     }
 }
